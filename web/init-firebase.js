8a4f87b725c8a9ca189ea8ae411b89a3751c7a3d
Andre Soares <andregsilv@gmail.com>
2020-11-27 17:45:22 -0300

    web

---
+++ web/init-firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD2YbQPX_uzEuCmCTh6Zu4leOzIkz9-ntM",
+    authDomain: "iasd-vila-yara.firebaseapp.com",
+    databaseURL: "https://iasd-vila-yara.firebaseio.com",
+    projectId: "iasd-vila-yara",
+    storageBucket: "iasd-vila-yara.appspot.com",
+    messagingSenderId: "649460426260",
+    appId: "1:649460426260:web:e6cc52cc73b6a6dbd4e901"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
